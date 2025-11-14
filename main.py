b6bd113d4f79d22786b9bd554121e16ae813d2e0
Usuaria/o para exames 1ª Avaliación DAM 2025-2026 <a25exame-dam-1@teis.intranet>
2025-11-14 16:34:57 +0100

    fa1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-System.out.println("Fase inicial del proyecto")
+public class Main {
+public static void main(String[] args) {
+
+System.out.println("Fase inicial del proyecto");
+System.Out.println("Desarrollado en Alumno");
+  }
+}
